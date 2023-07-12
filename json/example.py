5897cc417240d0e394d800f560cb617119a4edea
bbandari27 <89539516+bbandari27@users.noreply.github.com>
2023-07-12 16:21:45 -0400

    Update example.py

--- json/example.py
+++ json/example.py
@@ -1,30 +1,23 @@
-import pandas as pd
+import json
 
-# JSON response
-json_response = {
-    "isLastPage": True,
-    "limit": 25,
-    "size": 8,
-    "start": 0,
-    "values": [
-        # JSON data
-    ]
-}
-
-# Extract values from JSON
-values = json_response["values"]
-
-# Create an empty DataFrame
-data = pd.DataFrame()
-
-# Iterate over the values and populate the DataFrame
-for item in values:
-    project_permissions = {
-        "Project": item["scope"]["resourceId"],
-        "Permission Users": [user["displayName"] for user in item["users"]],
-        "Permission Groups": item["groups"]
+def convert_json(json_data):
+    converted_data = {
+        'groups': json_data['groups'],
+        'matcher': {
+            'displayId': json_data['matcher']['displayId'],
+            'id': json_data['matcher']['id'],
+            'type': {
+                'id': json_data['matcher']['type']['id'],
+                'name': json_data['matcher']['type']['name']
+            }
+        },
+        'type': json_data['type'],
+        'users': [user['name'] for user in json_data['users']]
     }
-    data = data.append(project_permissions, ignore_index=True)
+    return converted_data
 
-# Display the DataFrame
-print(data)
+# Convert JSON data
+converted_json = convert_json(json_data)
+
+# Print converted JSON
+print(json.dumps(converted_json, indent=4))
